0009514553bb07fdbfa94a77de09e05bc3e88da4
idrlykforeal <100017195+idrlykforeal@users.noreply.github.com>
2022-08-26 12:21:03 +0900

    Update Kris_base_converter_non_array_ver.py

--- Kris_base_converter_non_array_ver.py
+++ Kris_base_converter_non_array_ver.py
@@ -1,32 +1,33 @@
-#base converter-Kris Ni
-#this number converts positive base 10 integers to any base 2 to 9 integers.
+# base converter-Kris Ni
+# this number converts positive base 10 integers to any base 2 to 9 integers.
 
-num= int(input('input a positive integer in base 10: '))
-base= int(input('input base you need from 2 to 9: '))
-dev= int(num/base)
-mod= num%base
-out=mod
-#initialize variables
+num = int(input('input a positive integer in base 10: '))
+base = int(input('input base you need from 2 to 9: '))
+dev = int(num / base)
+mod = num % base
+out = mod
+# initialize variables
 
-i=1
-#initialize cursor
+i = 1
+# initialize cursor
 
-if num<0:
+if num < 0:
     print('please input a positive integer')
-if base<2 or base==10:
+    exit()
+if base < 2 or base == 10:
     print('base not invalid, please choose another applicable base that is not 10')
+    exit()
+# check for invalid input and base.
 
-#check for invalid input and base.
-    
-while dev>base-1:
+while dev > base - 1:
     mod = dev % base
-    dev=int(dev/base)
-    out=out+mod*(10**i)
-    i=i+1
-out=out+dev*(10**i)
+    dev = int(dev / base)
+    out = out + mod * (10 ** i)
+    i = i + 1
+out = out + dev * (10 ** i)
 
-#calculate
+# calculate
 
-print("the base",base," of ",num, "is: ", out)
+print("the base", base, " of ", num, "is: ", out)
 
-#output
+# output
